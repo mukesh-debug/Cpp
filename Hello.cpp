634a760a908b28c85994b8c5c62a59e7d749b26a
Mukesh Kumar <mukeshmaxkumar2000@gmail.com>
2021-07-12 18:11:30 +0530

    Added first cpp script file and its compiled version .out file

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+/*
+This script is my first script in c++ after a long time.
+*/
+#include<stdio.h>
+#include<iostream>
+
+int main()
+{
+  char str[]="Hey You!\n";
+  char name[20];
+  std::cout<<str<<"Who?\n";
+  std::cout<<"Enter your Name\n";
+  std::cin>>name;
+  std::cout<<"Hello, "<<name<<"\n";
+  printf("Hello, World\n");
+
+}
