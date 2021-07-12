40e971c444dca2430ce69208b680e444fb927677
Mukesh Kumar <mukeshmaxkumar2000@gmail.com>
2021-07-12 18:40:57 +0530

    added more lines of code in Hello.cpp file.

--- Hello.cpp
+++ Hello.cpp
@@ -13,5 +13,22 @@ int main()
   std::cin>>name;
   std::cout<<"Hello, "<<name<<"\n";
   printf("Hello, World\n");
+  int age;
+  char ch;
+  std::cout<<"Do you want to know about yourself(Y/N)\n";
+  std::cin>>ch;
+  if(ch=='y' or 'Y')
+  {
+    std::cout<<"Enter your age\n";
+    std::cin>>age;
+    if(age>=13 and age<19)
+      std::cout<<"You are a Teenager\n";
+    else if(age<13)
+      std::cout<<"You are a child\n";
+    else
+      std::cout<<"You are a Grown Up\n";
+
+  }
+
 
 }
